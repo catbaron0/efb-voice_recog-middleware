185183014cf494a44ebcc8f09ab2270efd7df079
Eana Hufwe <ilove@1a23.com>
2019-12-03 21:46:17 +0800

    fix: update Baidu speech API to the latest specs

--- efb_voice_recog_middleware/__init__.py
+++ efb_voice_recog_middleware/__init__.py
@@ -137,7 +137,18 @@ class BaiduSpeech(SpeechEngine):
     access_token: str = None
     full_token = None
     engine_name: str = "Baidu"
-    lang_list = ['zh', 'ct', 'en']
+    lang_list = ['zh', 'en', 'zh-yue', 'zh-x-en',
+                 'zh-x-sichuan', 'zh-x-farfield']
+
+    languages = {
+        "zh": 1537,
+        "zh-x-en": 1536,
+        "en": 1737,
+        "ct": 1637,  # for compatibility
+        "zh-yue": 1637,
+        "zh-x-sichuan": 1837,
+        "zh-x-farfield": 1936
+    }
 
     def __init__(self, channel: int, key_dict: Dict[str, str]):
         self.channel = channel
@@ -167,26 +178,25 @@ class BaiduSpeech(SpeechEngine):
         if lang.lower() not in self.lang_list:
             return ["ERROR!", "Invalid language."]
 
-        audio = pydub.AudioSegment.from_file(file)
-        audio = audio.set_frame_rate(16000)
-        d = {
-            "format": "pcm",
-            "rate": 16000,
-            "channel": self.channel,
-            "cuid": "testing_user",
-            "token": self.access_token,
-            "lan": lang,
-            "len": len(audio.raw_data),
-            "speech": base64.b64encode(audio.raw_data).decode()
-        }
-        r = requests.post("http://vop.baidu.com/server_api", json=d)
-        if r.status_code != 200:
-            return [r.content, r]
-        rjson = r.json()
-        if rjson['err_no'] == 0:
-            return [rjson['result']]
-        else:
-            return ["ERROR!", rjson['err_msg']]
+        audio = pydub.AudioSegment.from_file(file).set_frame_rate(16000).set_channels(1)
+        with BytesIO() as f:
+            audio.export(f, format="s16le", codec="pcm_s16le")
+            headers = {
+                "Content-Type": "audio/pcm;rate=16000"
+            }
+            params = {
+                "cuid": "catbaron.voice_recog",
+                "token": self.access_token,
+                "dev_pid": self.languages[lang],
+            }
+            r = requests.post("http://vop.baidu.com/server_api", params=params, headers=headers, data=f)
+            if r.status_code != 200:
+                return ["ERROR!", r.status_code, r.content]
+            rjson = r.json()
+            if rjson['err_no'] == 0:
+                return rjson['result']
+            else:
+                return ["ERROR!", rjson['err_msg']]
 
 
 class AzureSpeech(SpeechEngine):
